7d47ed48e7babf8adaa3aa4b2c92776774dfe912
Mishka1012 <mikhailamshei@grodnoapps.com>
2019-12-13 10:22:53 -0600

    First Commit

---
+++ InspoQuotes/K.swift
@@ -0,0 +1,13 @@
+//
+//  K.swift
+//  InspoQuotes
+//
+//  Created by Mikhail Amshei on 12/13/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let qCellID = "QuoteCell"
+}
